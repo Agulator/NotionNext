9c3dcd870f33618b4f0296fa9c3213d4576728ea
Agulator <1213931354@qq.com>
2025-12-12 10:48:53 +0800

    Change music player settings to true and auto play to false

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -25,10 +25,10 @@ module.exports = {
   SPOILER_TEXT_TAG: process.env.NEXT_PUBLIC_SPOILER_TEXT_TAG || '', // Spoiler文本隐藏功能，如Notion中 [sp]希望被spoiler的文字[sp]，填入[sp] 即可
 
   // 音乐播放插件
-  MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || false, // 是否使用音乐播放插件
+  MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || true, // 是否使用音乐播放插件
   MUSIC_PLAYER_VISIBLE: process.env.NEXT_PUBLIC_MUSIC_PLAYER_VISIBLE || true, // 是否在左下角显示播放和切换，如果使用播放器，打开自动播放再隐藏，就会以类似背景音乐的方式播放，无法取消和暂停
   MUSIC_PLAYER_AUTO_PLAY:
-    process.env.NEXT_PUBLIC_MUSIC_PLAYER_AUTO_PLAY || true, // 是否自动播放，不过自动播放时常不生效（移动设备不支持自动播放）
+    process.env.NEXT_PUBLIC_MUSIC_PLAYER_AUTO_PLAY || false, // 是否自动播放，不过自动播放时常不生效（移动设备不支持自动播放）
   MUSIC_PLAYER_LRC_TYPE: process.env.NEXT_PUBLIC_MUSIC_PLAYER_LRC_TYPE || '0', // 歌词显示类型，可选值： 3 | 1 | 0（0：禁用 lrc 歌词，1：lrc 格式的字符串，3：lrc 文件 url）（前提是有配置歌词路径，对 meting 无效）
   MUSIC_PLAYER_CDN_URL:
     process.env.NEXT_PUBLIC_MUSIC_PLAYER_CDN_URL ||
@@ -42,21 +42,21 @@ module.exports = {
       url: 'https://music.glj520.eu.org/%E9%99%88%E5%B0%8F%E6%98%A5-%E6%88%91%E4%B8%8D%E6%98%AF%E4%BC%9F%E4%BA%BA.mp3',
       cover:
         'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
-    }
+    },
     {
       name: '经过(独唱版) - 陈楚生',
       artist: 'Falcom Sound Team jdk',
       url: 'https://music.glj520.eu.org/%E7%BB%8F%E8%BF%87(%E7%8B%AC%E5%94%B1%E7%89%88)%20-%20%E9%99%88%E6%A5%9A%E7%94%9F.mp3',
       cover:
         'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
-    }
+    },
     {
       name: '葛漂亮-烟雨唱扬州(女版)',
       artist: 'Falcom Sound Team jdk',
       url: 'https://music.glj520.eu.org/%E8%91%9B%E6%BC%82%E4%BA%AE-%E7%83%9F%E9%9B%A8%E5%94%B1%E6%89%AC%E5%B7%9E(%E5%A5%B3%E7%89%88).mp3',
       cover:
         'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
-    }
+    },
     {
       name: '你瞒我瞒 - 陈柏宇',
       artist: 'Falcom Sound Team jdk',
